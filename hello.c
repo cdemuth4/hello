756c60ae24057bab4cd66d023657799f8f7f3f8f
Carter Demuth <cdemuth4@huskers.unl.edu>
2022-08-25 15:10:33 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/*
+*  Authors: Carter Demuth, Shaye Hoyle
+*  Date: 8/25/2022
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, I am Carter Demuth!\n");
+    printf("My major is Computer Engineering.\n");
+    printf("Hello World, I am Shaye Hoyle!\n");
+    printf("My major is Undecided.\n");
+
+    return 0;
+
+}
